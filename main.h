569bc92e78532202fa66759a8398ba5971bb3637
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-04-25 16:54:45 +0100

    files uploaded.

--- main.h
+++ main.h
@@ -1,11 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-#ifndef _MAIN_H_
-#define _MAIN_H_
 
 /**** FUNCTIONS ****/
 int _printf(const char *format, ...);
