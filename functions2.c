83a158f5ec5d598f863762deb41fc1ffaa480a01
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-04-28 20:41:14 +0100

    Files uploaded.

--- functions2.c
+++ functions2.c
@@ -7,14 +7,15 @@
  */
 int print_binary(va_list list)
 {
-    int num = va_arg(list, int), printed = 0, len;
-    char *binary = calloc((size_t) BUF_SIZE, sizeof(char));
+	int num = va_arg(list, int), printed = 0, len;
+	char *binary = calloc((size_t) BUF_SIZE, sizeof(char));
 
-    num = ABS(num);
-    binary = num_to_bin(num);
-    if (!binary)
-        return (0);
-    len = strlen(binary);
-    printed = write(1, &binary[0], len);
-    return (printed);
+	num = ABS(num);
+	binary = num_to_bin(num);
+	if (!binary)
+		return (0);
+	len = strlen(binary);
+	printed = write(1, &binary[0], len);
+
+	return (printed);
 }
